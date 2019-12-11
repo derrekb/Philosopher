3c9c6fa01fcef0c5849da02825c91ffd18b4c058
derrekb <45970048+derrekb@users.noreply.github.com>
2019-12-11 18:14:45 -0500

    Add files via upload

--- Philosopher.java
+++ Philosopher.java
@@ -5,19 +5,32 @@ import java.util.Random;
 
 public class Philosopher<X> extends Thread {
 	
+	//defining the semaphores, left, right, and screen
 	Semaphore l, r,s;
 	
+	//a philosopher is composed of a string, screen semaphore, and left and right chopstick semaphore
 	public Philosopher(String n,Semaphore screenSemaphore, Semaphore Lchopstick, Semaphore Rchopstick){
+		
+		  //super is used to invoke the parent class method 
 			super (n); s = screenSemaphore; l = Lchopstick; r = Rchopstick; }
 
 		@SuppressWarnings("unchecked")
 
+		//main function to run
 		public void run()	{ 
 			while (true){ 
+				
+				//the instances of thinking, hungry, and eating are random and we need to define a new random
 				Random R = new Random();
+				
 				if(R.nextInt(1001)<500) {
+					
+					//try to acquire the screen semaphore
 					try {s.acquire(); }
+					//once they have the screen they become hungry
 					catch(InterruptedException e) {}
+					
+					//if the .getName is equal to the philosopher name then print out that they are hungry since they have the screen semaphore
 					if(this.getName()=="Aristotle ") {
 						System.out.println("Hungry");}
 						if(this.getName()=="Plato ") {
@@ -28,11 +41,13 @@ public class Philosopher<X> extends Thread {
 							System.out.println("             "+"             "+"             Hungry");}
 						if(this.getName()=="Leibniz ")
 						{System.out.println("             "+"             "+"             "+"             Hungry");}
-					s.release();
-					
-					
+				//release the screen semaphore
+						s.release();
+				
+					//trying to acquire all 3 semaphores
 					try {s.acquire(); r.acquire(); l.acquire(); }
 					catch(InterruptedException e) {} 
+					//once a philosopher has all three they can eat
 					if(this.getName()=="Aristotle ") {
 						System.out.println("Eating");}
 						if(this.getName()=="Plato ") {
@@ -45,12 +60,14 @@ public class Philosopher<X> extends Thread {
 						{System.out.println("             "+"             "+"             "+"             Eating");}
 						try {
 							Thread.sleep(1000);
-						} catch (InterruptedException e1) {e1.printStackTrace();}  
+						} catch (InterruptedException e1) {e1.printStackTrace();}
+						//once they have eaten, all the semaphores can be released
 						s.release();r.release(); l.release();
 						
-						
+						//try to acquire the screen semaphore again
 					try {s.acquire();}
 					catch(InterruptedException e) {} 
+					//once they're done eating they go to back to thinking
 					if(this.getName()=="Aristotle ") {
 						System.out.println("Thinking");}
 					if(this.getName()=="Plato ") {
@@ -60,6 +77,7 @@ public class Philosopher<X> extends Thread {
 					if(this.getName()=="Kant ") {
 						System.out.println("             "+"             "+"             Thinking");}
 					if(this.getName()=="Leibniz "){System.out.println("             "+"             "+"             "+"             Thinking");}
+					//once the thinking has been printed out the three semaphores can be released.
 					s.release();r.release(); l.release();
 					
 					} 
--- Table.java
+++ Table.java
@@ -4,7 +4,10 @@ import java.util.concurrent.Semaphore;
 
 public class Table {
 	
-	
+	/*defining the semaphores. 
+	 we need a sempaphore for each chopstick and a screen
+	 
+	 */
 	private static Semaphore chopstick1 = new Semaphore(1);
 	private static Semaphore chopstick2 = new Semaphore(1);
 	private static Semaphore chopstick3 = new Semaphore(1);
@@ -15,15 +18,20 @@ public class Table {
 	
 	
 	public static void main(String[] args) {
-		
+		/*defining our threads. Each philosopher is their own thread.
+		Each philosopher has a name, a screen, and 2 chopsticks that are specific to their location
+		*/
 		Thread Aristotle = new Philosopher<String>("Aristotle ",screenSemaphore, chopstick1, chopstick2) ;
 		Thread Plato = new Philosopher<String>("Plato ",screenSemaphore, chopstick2, chopstick3) ;
 		Thread Socrates = new Philosopher<String>("Socrates ",screenSemaphore, chopstick3, chopstick4) ;
 		Thread Kant = new Philosopher<String>("Kant ",screenSemaphore, chopstick4, chopstick5) ;
 		Thread Leibniz = new Philosopher<String>("Leibniz ",screenSemaphore, chopstick5, chopstick1) ;
+		
+		//They all start out thinking first
 		System.out.println("Phil 0        Phil 1       Phil 2      Phil 3        Phil 4");
 		System.out.println("Thinking      Thinking	   Thinking    Thinking      Thinking");
 		
+		//starting each of the philosophers up
 		Aristotle.start();
 		Plato.start();
 		Socrates.start();
