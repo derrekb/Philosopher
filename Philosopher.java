661e1c2e5af8e9fb8c7c68b6fad2e95b78fcbfe2
Emanoid <56281174+Emanoid@users.noreply.github.com>
2019-12-11 02:06:30 -0500

    Update Philosopher.java

--- Philosopher.java
+++ Philosopher.java
@@ -3,52 +3,47 @@ package Din_Phil_Quiz;
 import java.util.concurrent.Semaphore;
 import java.util.Random;
 
-public class Philosopher<X> extends Thread implements IPhilosopher<X>{
+public class Philosopher<X> extends Thread {
 	
 	Semaphore l, r,s;
 	
 	public Philosopher(String n,Semaphore screenSemaphore, Semaphore Lchopstick, Semaphore Rchopstick){
-			super (n);s = screenSemaphore; l = Lchopstick; r = Rchopstick; }
+			super (n); s = screenSemaphore; l = Lchopstick; r = Rchopstick; }
 
 		@SuppressWarnings("unchecked")
 
 		public void run()	{ 
+			try {s.acquire(); } 
+			catch(InterruptedException e) {}
 			System.out.println(this.getName()+"Thinking");
-			int I = 1;
-			while (I==1){ 
-				try {s.acquire(); } catch(InterruptedException e) {}
-					System.out.println(this.getName()+"Thinking");
-					Random R = new Random();
-					if(R.nextInt(1001)<100) {
-					System.out.println(this.getName()+"Hungry");	
-					try {r.acquire(); l.acquire(); 
-						System.out.println(this.getName()+"Eating");
-					} catch(InterruptedException e) {} }
+			s.release();
+			
+			while (true){ 
+				Random R = new Random();
+				if(R.nextInt(1001)<50) {
+					try {s.acquire(); }
+					catch(InterruptedException e) {}
+					System.out.println(this.getName()+"Hungry");
+					s.release();
 					
-			s.release();r.release(); l.release();
-	} }
+					try {s.acquire();r.acquire(); l.acquire(); }
+					catch(InterruptedException e) {} 
+					System.out.println(this.getName()+"Eating");
+						try {
+							Thread.sleep(1000);
+						} catch (InterruptedException e1) {e1.printStackTrace();}
+					s.release();r.release(); l.release();
+											
+					try {s.acquire();}
+					catch(InterruptedException e) {} 
+					System.out.println(this.getName()+"Thinking");
+					s.release();
+					} 
+					
+					
+					
+			
+	}}} 	
+
 		
-		@Override
-		public boolean hasLeft() {
-			// TODO Auto-generated method stub
-			return false;
-		}
-
-		@Override
-		public boolean hasRight() {
-			// TODO Auto-generated method stub
-			return false;
-		}
-
-		@Override
-		public boolean hasEaten() {
-			// TODO Auto-generated method stub
-			return false;
-		}
-
-		@Override
-		public boolean isHungry() {
-			// TODO Auto-generated method stub
-			return false;
-		}}
 
